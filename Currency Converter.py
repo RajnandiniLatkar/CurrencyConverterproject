a86f0c6a78ca5f4a430371f072549d40dd224d6a
Rajnandini <130739610+RajnandiniLatkar@users.noreply.github.com>
2023-07-18 20:41:51 +0530

    Create Currency Converter.py

---
+++ Currency Converter.py
@@ -0,0 +1,34 @@
+from currency_converter import CurrencyConverter
+import tkinter as tk
+c= CurrencyConverter()
+#print(c.convert(12,"USD","INR"))
+
+window=tk.Tk()
+window.geometry("500x360")
+window.title("Currency Converter")
+
+def clicked():
+    amount=int(entry1.get())
+    cur1=entry2.get()
+    cur2=entry3.get()
+    data=c.convert(amount,cur1,cur2)
+    Label4=tk.Label(window,text=data,font="Times 16 bold").place(x=200,y=300)
+
+
+Label=tk.Label(window,text="Currency Converter",font="Times 20 bold").place(x=120,y=40)
+
+Label1=tk.Label(window,text="Enter amount here:",font="Times 16 bold").place(x=70,y=100)
+entry1=tk.Entry(window)
+
+Label2=tk.Label(window,text="Enter your currency here:",font="Times 16 bold").place(x=30,y=150)
+entry2=tk.Entry(window)
+
+Label3=tk.Label(window,text="Enter your desired currency:",font="Times 16 bold").place(x=15,y=200)
+entry3=tk.Entry(window)
+
+Button=tk.Button(window,text="Click",font="Times 16 bold",command=clicked).place(x=220,y=250)
+entry1.place(x=270,y=105)
+entry2.place(x=270,y=155)
+entry3.place(x=270,y=205)
+
+window.mainloop()
